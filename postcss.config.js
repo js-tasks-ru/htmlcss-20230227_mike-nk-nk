d0d6daeab1a37b8a2899bf0bf48bb1c69d5f9ec1
Mikhail Moshkevich <mike.nk.nk@gmail.com>
2023-04-10 15:21:49 +0300

    Прогнал все через postcss и prettier

---
+++ postcss.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    //syntax: "postcss-scss",
+    //map: { inline: true },
+    plugins: [
+        //require("postcss-import"),
+        //require("postcss-nested"),
+        //require("postcss-advanced-variables"),
+        //require("postcss-strip-inline-comments"),
+        require('autoprefixer'),
+        //require("cssnano"),
+        //require("prettier"),
+    ],
+};
